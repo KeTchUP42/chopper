9463ed1ec6c139234676bd8e2f77b6da9671d00f
Roman Bondarenko <rom_bon@mail.ru>
2020-06-27 15:11:11 +0300

    1. DirtMixerCell fixed and improved. 2. ErrorException added.

--- src/Gear/Handling/MixerCell/DirtMixerCell.php
+++ src/Gear/Handling/MixerCell/DirtMixerCell.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 
 namespace Chopper\Gear\Handling\MixerCell;
 
+use Chopper\Exceptions\ErrorException;
 use Chopper\Gear\Handling\MixerCell\MixerCellEssence\AbstractMixerCell;
 use RecursiveArrayIterator;
 use RecursiveIteratorIterator;
@@ -14,17 +15,21 @@ use RecursiveIteratorIterator;
 class DirtMixerCell extends AbstractMixerCell
 {
     /**
-     * Result wrapp
+     * Result wrap
      */
-    private const HTML_WRAPP_1 = "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<meta charset=\"UTF-8\">\n<title>Test</title>\n</head>\n<body>\n";
-    private const HTML_WRAPP_2 = "\n</body>\n</html>";
+    private const HTML_WRAP_1 = "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<meta charset=\"UTF-8\">\n<title>Result</title>\n</head>\n<body>\n";
+    private const HTML_WRAP_2 = "\n</body>\n</html>";
 
     /**
      * @inheritDoc
      */
     public function handle(): string
     {
-        return $this->shuffle_merge();
+        $this->configuringWarningHandler();
+        $result = $this->shuffle_merge();
+        $this->restoreHandler();
+
+        return $result;
     }
 
     /**
@@ -36,7 +41,11 @@ class DirtMixerCell extends AbstractMixerCell
     {
         $data = [];
         foreach ($this->files as $file) {
-            $fileData = unserialize($file->read(), [false]);
+            try {
+                $fileData = unserialize($file->read(), [false]);
+            } catch (ErrorException $error) {
+                continue;
+            }
             if (is_array($fileData)) {
                 $data[] = array_merge($data, $fileData);
             }
@@ -44,6 +53,25 @@ class DirtMixerCell extends AbstractMixerCell
         $data = iterator_to_array(new RecursiveIteratorIterator(new RecursiveArrayIterator($data)), false);
         shuffle($data);
 
-        return self::HTML_WRAPP_1.implode('', $data).self::HTML_WRAPP_2;
+        return self::HTML_WRAP_1.implode('', $data).self::HTML_WRAP_2;
+    }
+
+    /**
+     * Method configures error handler
+     */
+    private function configuringWarningHandler(): void
+    {
+        set_error_handler(static function ($severity, $message, $file, $line) {
+            throw new  ErrorException($message, $severity, $severity, $file, $line);
+        }
+        );
+    }
+
+    /**
+     * Method restores error handler
+     */
+    private function restoreHandler(): void
+    {
+        restore_error_handler();
     }
 }
